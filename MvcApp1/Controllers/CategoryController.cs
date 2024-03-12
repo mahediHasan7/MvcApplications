9dcf13430f907f11fe367e1a580012ad1d73c04b
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2024-03-12 10:54:33 +0800

    first implementation of Category controller

---
+++ MvcApp1/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApp1.Data;
+using MvcApp1.Models;
+
+namespace MvcApp1.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        // Retrieve the data from database
+        List<Category> categories = _db.Categories.ToList();
+
+        return View(categories);
+    }
+
+}
