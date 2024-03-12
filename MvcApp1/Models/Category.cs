24bf2c906324dfdac3929be1db8b6ee7a01ec939
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2024-03-12 11:03:43 +0800

    added category model file

---
+++ MvcApp1/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace MvcApp1.Models;
+
+[DebuggerDisplay("{Name}")]
+public class Category
+{
+    public int Id { get; set; }
+    [Required]
+    public required string Name { get; set; }
+    public int DisplayOrder { get; set; }
+
+}
