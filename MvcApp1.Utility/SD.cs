a07d0c6672843ad079f2de2500fcc39f8102a3e9
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2024-04-06 14:47:18 +0800

    various statuses added in the static file

--- MvcApp1.Utility/SD.cs
+++ MvcApp1.Utility/SD.cs
@@ -7,4 +7,16 @@ public static class SD
     public const string Role_Employee = "Employee";
     public const string Role_Customer = "Customer";
     public const string Role_Company = "Company";
+
+    public const string StatusPending = "Pending";
+    public const string StatusApproved = "Approved";
+    public const string StatusInProcess = "InProcess";
+    public const string StatusShipped = "Shipped";
+    public const string StatusCancelled = "Cancelled";
+    public const string StatusRefunded = "Refunded";
+
+    public const string PaymentStatusPending = "Pending";
+    public const string PaymentStatusApproved = "Approved";
+    public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+    public const string PaymentStatusRejected = "Rejected";
 }
