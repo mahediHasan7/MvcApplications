7a1ec41dcc35afa65eebd1d23c29ebb40006c283
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2024-04-06 14:43:07 +0800

    order js and png file added

---
+++ MvcApp1/wwwroot/js/order.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall' },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phone', "width": "20%" },
+            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: null,
+                "render": function (data, type, row) {
+                    return `
+                    <div  class="w-75 btn-group" role="group">
+                      <a class="btn btn-primary mx-2" href="/admin/order/details?orderId=${data.id}"><i class="bi bi-pencil-square"></i></a>
+                    </div>
+                ` },
+                "width": "10%"
+            }
+        ]
+    });
+}
